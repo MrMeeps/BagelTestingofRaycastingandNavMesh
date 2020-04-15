d1187eafc8217ab0825ae246fa215c05b89c68ef
MrMeeps <56411666+MrMeeps@users.noreply.github.com>
2020-04-15 12:03:03 -0700

    testing

---
+++ Assets/SampleOne.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SampleOne : MonoBehaviour
+{
+    public Transform target;
+
+   UnityEngine.AI.NavMeshAgent agent;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetCompoent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.SetDestination(target.position);
+    }
+}
---
+++ Assets/TestOne.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.InteropServices;
+using UnityEngine;
+
+public class TestOne : MonoBehaviour
+{
+
+    public LayerMask mask;
+
+    // Update is called once per frame
+    void Update()
+    {
+        Ray ray = new Ray(transform.position, transform.forward);
+        RaycastHit hitInfo;
+        if (Physics.Raycast(ray, out hitInfo, 200, mask, QueryTriggerInteraction.Ignore))
+        {
+            print(hitInfo.collider.gameObject.name);
+            Destroy(hitInfo.transform.gameObject);
+            Debug.DrawLine(ray.origin, hitInfo.point, Color.red);
+        }
+        else
+        {
+            Debug.DrawLine(ray.origin, ray.origin + ray.direction * 100, Color.green);
+        }
+    }
+}
